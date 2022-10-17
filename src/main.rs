1aa289744b3c380a5a8b2ea45cd77079eac4b9f4
GaryCraft <garycraft@our-space.xyz>
2022-10-17 10:52:22 -0500

    Start API

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#[macro_use] extern crate rocket;
+
+
+#[launch]
+fn rocket() -> _ {
+	rocket::build()
+		.mount("/", routes![index])
+}
+
+#[get("/")]
+fn index() -> &'static str {
+	"Hello, world!"
+}
