aaba7caded333e91b7045a141b45507aab91333a
GaryCraft <garycraft@our-space.xyz>
2022-10-18 21:52:21 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 #[macro_use] extern crate rocket;
+pub mod routes;
+pub mod config;
+pub mod util;
 
-
-#[launch]
-fn rocket() -> _ {
-	rocket::build()
-		.mount("/", routes![index])
+#[derive(Debug)]
+pub struct App {
+	pub config: config::Config,
 }
 
-#[get("/")]
-fn index() -> &'static str {
-	"Hello, world!"
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+	// Load the configuration file
+	let config = config::load_config();
+	// Create the application
+	let app = App { config };
+	// Start the server
+	let _rocket = rocket::build()
+		.manage(app)
+		.mount("/", routes![routes::index])
+		.mount("/wake", routes![routes::wake::wol_wake])
+		.launch()
+		.await?;
+	// Return the result
+	Ok(())
 }
